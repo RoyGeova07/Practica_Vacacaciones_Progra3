2750d484bb279910996068346b6249446c8bef19
RoyGeova07 <royumana3@gmail.com>
2024-12-27 00:21:57 -0600

    DIA 3

---
+++ Bucles/do_whileee.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(){
+	
+	int i;
+	
+	do{
+		
+		cout<<i<<endl;
+		i++;
+		
+	}while(i<=10);
+	
+	system("pause");
+	return 0;
+	
+}
---
+++ Bucles/whilee.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#include <conio.h>//libreria para que el ejecutable exe no se cierre completamente
+
+int main(){
+	
+	
+	int i=10;
+	
+	while(i>=1){
+		
+		cout<<i<<endl;
+		i--;
+		 
+	}
+	getch();
+	return 0;
+}
+
